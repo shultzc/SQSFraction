454a34d177b964cad19e246ad0b33c8d07798e5e
Conrad Shultz <conrad@synthetiqsolutions.com>
2012-03-15 16:05:45 -0700

    Added Copyright statements to demo project.

--- FractioniPadDemo/FractioniPadDemo/main.m
+++ FractioniPadDemo/FractioniPadDemo/main.m
@@ -3,7 +3,7 @@
 //  FractioniPadDemo
 //
 //  Created by Conrad Shultz on 3/15/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Synthetiq Solutions LLC. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
